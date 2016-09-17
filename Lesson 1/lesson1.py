6102b9e6dacf8c460db5aaecc89410c73bf21163
Stam Kaly <stambugatti@gmail.com>
2016-09-17 19:31:51 +0300

    Added lesson 1

---
+++ Lesson 1/lesson1.py
@@ -0,0 +1,54 @@
+# Integers
+x = 1
+
+
+
+# Floats
+y = 1.1029
+
+
+
+# Strings
+z = "Something"
+print(z)
+
+
+
+# String vs integer
+x = 1
+y = x + 3 # Using a sting is incorrect
+print(y)
+
+
+
+# If statements
+x = 1
+if x == 21:
+    print("To x einai 21 re malaka")
+x = 2
+if x == 2:
+    print("2")
+
+
+
+
+# Bool values
+x = True
+if x == True:
+    print("x is True!!!!")
+    x = False
+if x == True:
+    print("x is True")
+
+
+
+
+x = False
+
+
+
+
+x = 1
+x += 1
+if x == 2:
+    print ("eimai pro")
